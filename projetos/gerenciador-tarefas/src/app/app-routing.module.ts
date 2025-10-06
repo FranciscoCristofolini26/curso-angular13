e5eb47309c941c11975e662bd7b43d9138b3eb32
FranciscoCristofolini26 <francisco.minatti.cristofolini@gmail.com>
2025-10-06 13:46:37 -0300

    .

---
+++ projetos/gerenciador-tarefas/src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { TarefaRoutes } from './tarefas';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/tarefas/listar',
+    pathMatch: 'full'
+  },
+  ...TarefaRoutes
+];
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
---
+++ projetos/gerenciador-tarefas/src/app/tarefas/tarefas.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'
+import { TarefaService } from './shared';
+import { ListarTarefaComponent } from './listar';
+import { RouterModule } from '@angular/router';
+import { CadastrarTarefaComponent } from './cadastrar';
+
+
+
+@NgModule({
+  declarations: [
+
+  
+    ListarTarefaComponent,
+        CadastrarTarefaComponent
+  ],
+  imports: [
+    CommonModule, 
+    RouterModule,
+    FormsModule
+  ],
+  providers: [
+    TarefaService
+  ]
+})
+export class TarefasModule { }
